978c078af3143430e2c068f15bc9cf372663cf11
Dzikri Alan <dzikrialan0@gmail.com>
2023-11-19 10:58:16 +0700

    style: templating

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="main-footer">
+      <div class="container">
+          <div class="pull-right hidden-xs">
+              <b>Version</b> 2.4.0
+          </div>
+          <strong>Copyright &copy; 2023 <a href="http://dzikri-alan.vercel.app/">Dzikri Alan</a>.</strong> All rights
+          reserved.
+      </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
