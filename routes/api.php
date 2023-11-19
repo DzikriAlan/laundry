5dbb88254978f2bc9f02d17eece20e003626a025
Dzikri Alan <dzikrialan0@gmail.com>
2023-11-19 11:58:02 +0700

    feat: CRUD Outlets

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,5 @@ use Illuminate\Http\Request;
 Route::post('/login', 'Auth\LoginController@login');
 
 Route::group(['middleware' => 'auth:api'], function() {
+  Route::resource('/outlets', 'API\OutletController')->except(['show']);
 });
